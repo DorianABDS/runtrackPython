6d4778dfa1a5a0fd91b9e1c200dcac038e457f5d
Dorian ABBADESSA <dorian.abbadessa@laplateforme.io>
2024-11-06 20:30:26 +0100

    Add files via upload

---
+++ Python/test.py
@@ -0,0 +1,10 @@
+def main():
+    # wallet = 75
+    # produit = 50
+    # reste = wallet - produit
+    # print("Il vous reste " reste] "euros")
+    print(1+1)
+
+
+if __name__ == "__main__":
+    main()
